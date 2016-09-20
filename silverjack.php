b3a20efb70a0b732f7c67a727443fdcca4772ff5
Matt Johnson <matjohnson@csumb.edu>
2016-09-20 18:17:13 +0000

    starting names

--- silverjack.php
+++ silverjack.php
@@ -1,3 +1,49 @@
+<?php
+    $names = array(
+        "Misael Aguilar",
+        "Doug Alexander",
+        "Tristan Anderson",
+        "Erica Au",
+        Sonali Bharat
+        Lucas Childers
+        Jack Cousineau
+        Brandon Cruz
+        Ediberto Cruz Cruz
+        Luigi D'Antonio
+        Ben Dagg
+        Jessie Dowding
+        Cristian Galvan
+        Alan Garcia
+        Meya Gorbea
+        Bianca Jaramillo
+        Matt Johnson
+        Morgan Juran
+        Monique La Croix
+        Scott Ligon
+        Adrian Martinez
+        Sean McCarthy
+        Hardesh Mohar
+        Daniel Mora
+        Luis Mora
+        Bryant Ng
+        Chris Ngo
+        Tim Perry
+        Anna Pomelov
+        Bryan Rodriguez
+        Alex Ruvalcaba
+        Josh Shallow
+        Mateo Sixtos
+        Austin Thao
+        Garrett Tibbetts
+        Michael Vargas
+        Ulysses Vega
+        Edwin Yantis
+        
+    );
+
+>
+
+
 <!DOCTYPE html>
 
 <html>
