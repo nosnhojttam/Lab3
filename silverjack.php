c070ae9cf585340921bbee05907badc5af59990c
Adrian Martinez <admartinez@csumb.edu>
2016-09-20 18:21:49 +0000

    name update

--- silverjack.php
+++ silverjack.php
@@ -4,44 +4,42 @@
         "Doug Alexander",
         "Tristan Anderson",
         "Erica Au",
-        Sonali Bharat
-        Lucas Childers
-        Jack Cousineau
-        Brandon Cruz
-        Ediberto Cruz Cruz
-        Luigi D'Antonio
-        Ben Dagg
-        Jessie Dowding
-        Cristian Galvan
-        Alan Garcia
-        Meya Gorbea
-        Bianca Jaramillo
-        Matt Johnson
-        Morgan Juran
-        Monique La Croix
-        Scott Ligon
-        Adrian Martinez
-        Sean McCarthy
-        Hardesh Mohar
-        Daniel Mora
-        Luis Mora
-        Bryant Ng
-        Chris Ngo
-        Tim Perry
-        Anna Pomelov
-        Bryan Rodriguez
-        Alex Ruvalcaba
-        Josh Shallow
-        Mateo Sixtos
-        Austin Thao
-        Garrett Tibbetts
-        Michael Vargas
-        Ulysses Vega
-        Edwin Yantis
-        
-    );
+        "Sonali Bharat",
+        "Lucas Childers",
+        "Jack Cousineau",
+        "Brandon Cruz",
+        "Ediberto Cruz Cruz",
+        "Luigi D'Antonio",
+        "Ben Dagg",
+        "Jessie Dowding",
+        "Cristian Galvan",
+        "Alan Garcia",
+        "Meya Gorbea",
+        "Bianca Jaramillo",
+        "Matt Johnson",
+        "Morgan Juran",
+        "Monique La Croix",
+        "Scott Ligon",
+        "Adrian Martinez",
+        "Sean McCarthy",
+        "Hardesh Mohar",
+        "Daniel Mora",
+        "Luis Mora",
+        "Bryant Ng",
+        "Chris Ngo",
+        "Tim Perry",
+        "Anna Pomelov",
+        "Bryan Rodriguez",
+        "Alex Ruvalcaba",
+        "Josh Shallow",
+        "Mateo Sixtos",
+        "Austin Thao",
+        "Garrett Tibbetts",
+        "Michael Vargas",
+        "Ulysses Vega",
+        "Edwin Yantis");
 
->
+?>
 
 
 <!DOCTYPE html>
