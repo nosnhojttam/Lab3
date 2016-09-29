a0f423dcd268caab1dd5b76e44d1954a46947ef8
Matt Johnson <matjohnson@csumb.edu>
2016-09-29 18:28:47 +0000

    updating getHand function again ;-)

--- silverjack.php
+++ silverjack.php
@@ -55,7 +55,7 @@
             $total = 0;
             while($total < 42){
                 $card = rand(1,52);
-                if(($total+$card) <= 42){
+                if(($total+(($card%13 == 0 ? 13 : $card%13))) <= 42){
                     $total += $card % 13;
                     if($card < 14){
                         echo "<img src='img/cards/clubs/" . $card . ".png'>";
@@ -76,8 +76,12 @@
                         echo "<img src='img/cards/spades/" . ($card%13 == 0 ? 13 : $card%13) . ".png'>";
                     }
                 }
+                else{
+                    break;
+                }
             }
             $scores[] = $total;
+            echo "<span>".$total."</span>";
             echo "<br>";
         }
     }
