eb0c5864e5f26756ea98c3bee101ed8b6b88e8cc
Matt Johnson <matjohnson@csumb.edu>
2016-10-02 07:22:39 +0000

    fixed duplication of cards

--- silverjack.php
+++ silverjack.php
@@ -41,6 +41,7 @@
         
     $players = array();
     $scores = array();
+    $cards = array();
         
     function getPlayers($num){
         global $names, $players;
@@ -49,12 +50,15 @@
     }
     
     function getHand(){
-        global $names, $players, $scores;
+        global $names, $players, $scores, $cards;
         for($i = 0; $i < count($players); $i++){
             echo  "<h2>" . $names[$players[$i]] . "<h2/>";
             $total = 0;
             while($total < 42){
                 $card = rand(1,52);
+                while(in_array($card, $cards)){
+                    $card = rand(1,52);
+                }
                 if(($total+(($card%13 == 0 ? 13 : $card%13))) <= 42){
                     $total += $card % 13;
                     if($card < 14){
@@ -75,6 +79,7 @@
                         }
                         echo "<img src='img/cards/spades/" . ($card%13 == 0 ? 13 : $card%13) . ".png'>";
                     }
+                    $cards[] = $card;
                 }
                 else{
                     break;
